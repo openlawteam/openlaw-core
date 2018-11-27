c8832a8893ca0a6800eefbe7811167dda2bc38e1
Craig Blake <craigwblake@gmail.com>
2018-11-27 16:08:25 -0500

    Removing old printers.

--- shared/src/main/scala/org/adridadou/openlaw/parser/template/printers/HtmlAgreementPrinter.scala
+++
@@ -1,9 +0,0 @@
-package org.adridadou.openlaw.parser.template.printers
-
-trait HtmlAgreementPrinter extends AgreementPrinter[String] {
-
-  def noHtml(text:String):String = text
-    .replace(">","&gt;")
-    .replace("<", "&lt;")
-
-}
