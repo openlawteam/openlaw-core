843ca41df6938e382b285714daadd340b414b780
Jacqueline Outka <jacqueline@outka.xyz>
2019-01-17 16:28:46 -0500

    Annotation two types (#57)
    * there should be two types of annotation
    
    * fixed compilation error
    
    * fixing one test
    
    * looks like we may need the diff syntax
    
    * fixing test WIP
    
    * some test issues fixed
    
    * update syntax & names

--- shared/src/main/scala/org/adridadou/openlaw/parser/template/GlobalRules.scala
+++ shared/src/main/scala/org/adridadou/openlaw/parser/template/GlobalRules.scala
@@ -18,7 +18,8 @@ trait GlobalRules extends Parser {
   val openC = "<<"
   val closeC = ">>"
 
-  val openCloseAnnotation ="\"\"\""
+  val openCloseAnnotationHeader ="\'\'\'"
+  val openCloseAnnotationNote = "\"\"\""
 
   val sectionChar = "^"
 
@@ -42,15 +43,15 @@ trait GlobalRules extends Parser {
 
   def quote: Rule0 = rule {"“" | "\"" | "”" | "'" }
 
-  def loosenChar: Rule0 = rule { !(centered | rightThreeQuarters | right | pagebreak | indent | em | openCloseAnnotation) ~  ANY }
+  def loosenChar: Rule0 = rule { !(centered | rightThreeQuarters | right | pagebreak | indent | em | openCloseAnnotationHeader | openCloseAnnotationNote) ~  ANY }
 
-  def normalChar: Rule0 = rule { !( "|" | centered | rightThreeQuarters | right | pagebreak | indent | em | openS | closeS | openB | closeB | openC | closeC | openA | closeA | sectionChar | variableSectionChar | openCloseAnnotation) ~  ANY }
+  def normalChar: Rule0 = rule { !( "|" | centered | rightThreeQuarters | right | pagebreak | indent | em | openS | closeS | openB | closeB | openC | closeC | openA | closeA | sectionChar | variableSectionChar | openCloseAnnotationHeader | openCloseAnnotationNote) ~  ANY }
 
-  def normalCharNoReturn: Rule0 = rule { !( nl | "|" | centered | rightThreeQuarters | right | pagebreak | indent | em | openS | closeS | openB | closeB | openC | closeC | openA | closeA | sectionChar | variableSectionChar | openCloseAnnotation) ~  ANY }
+  def normalCharNoReturn: Rule0 = rule { !( nl | "|" | centered | rightThreeQuarters | right | pagebreak | indent | em | openS | closeS | openB | closeB | openC | closeC | openA | closeA | sectionChar | variableSectionChar | openCloseAnnotationHeader | openCloseAnnotationNote) ~  ANY }
 
   def commentsChar: Rule0 = rule {zeroOrMore(noneOf(nl))}
 
-  def normalCharNoColons: Rule0 = rule { !(colons | "|" | centered | rightThreeQuarters | right | pagebreak | indent | em | openS | closeS | openB | closeB | openC | closeC | openA | closeA | sectionChar | variableSectionChar | openCloseAnnotation) ~  ANY }
+  def normalCharNoColons: Rule0 = rule { !(colons | "|" | centered | rightThreeQuarters | right | pagebreak | indent | em | openS | closeS | openB | closeB | openC | closeC | openA | closeA | sectionChar | variableSectionChar | openCloseAnnotationHeader | openCloseAnnotationNote) ~  ANY }
 
   def characters: Rule0 = rule { oneOrMore(normalChar)  } // word
 
--- shared/src/test/scala/org/adridadou/openlaw/vm/OpenlawExecutionEngineSpec.scala
+++ shared/src/test/scala/org/adridadou/openlaw/vm/OpenlawExecutionEngineSpec.scala
@@ -722,17 +722,17 @@ class OpenlawExecutionEngineSpec extends FlatSpec with Matchers {
     }
   }
 
-  it should "handle annotation" in {
-    val startEndQuote = "\"\"\""
+  it should "handle header level annotations" in {
+    val startEndQuote = "\'\'\'"
     val template =
       compile(
         s"""
-          |before the annotation
-          |
+           |before the annotation
+           |
           |$startEndQuote
-          |this is some text for my annotation
-          |$startEndQuote
-          |
+           |this is some text for my annotation
+           |$startEndQuote
+           |
           |after the annotation
         """.stripMargin)
 
@@ -743,7 +743,34 @@ class OpenlawExecutionEngineSpec extends FlatSpec with Matchers {
         val text = parser.forReview(result.agreements.head,ParagraphEdits())
         text shouldBe """<p class="no-section"><br />before the annotation</p><p class="no-section"></p><p class="no-section">after the annotation<br />        </p>"""
         val text2 = parser.forPreview(result.agreements.head,ParagraphEdits())
-        text2 shouldBe """<div class="openlaw-paragraph paragraph-1"><p class="no-section"><br />before the annotation</p></div><div class="openlaw-paragraph paragraph-2"><p class="no-section"><span class="openlaw-annotation"><br />this is some text for my annotation<br /></span></p></div><div class="openlaw-paragraph paragraph-3"><p class="no-section">after the annotation<br />        </p></div>"""
+        text2 shouldBe """<div class="openlaw-paragraph paragraph-1"><p class="no-section"><br />before the annotation</p></div><div class="openlaw-paragraph paragraph-2"><p class="no-section"><span class="openlaw-annotation-header"><br />this is some text for my annotation<br /></span></p></div><div class="openlaw-paragraph paragraph-3"><p class="no-section">after the annotation<br />        </p></div>"""
+      case Left(ex) =>
+        fail(ex)
+    }
+  }
+
+  it should "handle note level annotations" in {
+    val startEndQuote = "\"\"\""
+    val template =
+      compile(
+        s"""
+           |before the annotation
+           |
+          |$startEndQuote
+           |this is some text for my annotation
+           |$startEndQuote
+           |
+          |after the annotation
+        """.stripMargin)
+
+
+    engine.execute(template, TemplateParameters(), Map()) match {
+      case Right(result) =>
+        result.state shouldBe ExecutionFinished
+        val text = parser.forReview(result.agreements.head,ParagraphEdits())
+        text shouldBe """<p class="no-section"><br />before the annotation</p><p class="no-section"></p><p class="no-section">after the annotation<br />        </p>"""
+        val text2 = parser.forPreview(result.agreements.head,ParagraphEdits())
+        text2 shouldBe """<div class="openlaw-paragraph paragraph-1"><p class="no-section"><br />before the annotation</p></div><div class="openlaw-paragraph paragraph-2"><p class="no-section"><span class="openlaw-annotation-note"><br />this is some text for my annotation<br /></span></p></div><div class="openlaw-paragraph paragraph-3"><p class="no-section">after the annotation<br />        </p></div>"""
       case Left(ex) =>
         fail(ex)
     }
