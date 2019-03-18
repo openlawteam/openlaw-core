f8de251f8a1de12795854ad049cc9091648f2147
Matthew Rothenberg <mroth@consensys.net>
2019-03-18 09:39:25 -0400

    more docs of what stages of coverage_report do

--- scripts/coverage_report.sh
+++ scripts/coverage_report.sh
@@ -3,6 +3,12 @@
 #
 # Dependencies:
 #   $CODACY_PROJECT_TOKEN
-sbt coverage test
+
+# run the tests and create coverage files
+sbt clean coverage test
+# export scoverage report files into cobertura compatible files
 sbt coverageReport
+# if you have sub-projects, you will want to aggregate all reports into one
 sbt coverageAggregate
+# upload results to Codacy (TODO: RE-ENABLE, CURRRENTLY DISABLED)
+# sbt codacyCoverage
