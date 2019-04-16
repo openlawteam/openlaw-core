3ed748269430c8a52995a8d6369c4d5bb4f612b3
Matthew Rothenberg <mroth@consensys.net>
2019-04-16 17:22:07 -0400

    Add upload results for coverage report

--- scripts/coverage_report.sh
+++ scripts/coverage_report.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 # Runs tests with coverage, and uploads final report to codacy.
 #
-# Dependencies:
-#   $CODACY_PROJECT_TOKEN
+# This takes significantly longer than just running the tests on their own, so
+# we tend to run this in a separate concurrent phase to enable fast test
+# failure.
 
 # run the tests and create coverage files
 sbt clean coverage test
@@ -10,5 +11,14 @@ sbt clean coverage test
 sbt coverageReport
 # if you have sub-projects, you will want to aggregate all reports into one
 sbt coverageAggregate
-# upload results to Codacy (TODO: RE-ENABLE, CURRRENTLY DISABLED)
-# sbt codacyCoverage
+
+# upload results to Codacy
+UPLOAD_RESULTS=${UPLOAD_RESULTS:-0}
+if [ "$UPLOAD_RESULTS" -eq "1" ]; then
+    if [ -z "$CODACY_PROJECT_TOKEN" ]; then
+        echo "No CODACY_PROJECT_TOKEN env variable! Can't upload results."
+        exit 1
+    else
+        sbt codacyCoverage
+    fi
+fi
