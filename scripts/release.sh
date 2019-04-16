c56c5345a366bb093f4ebd298e1e1758e26665ea
Matthew Rothenberg <mroth@consensys.net>
2019-04-16 16:55:37 -0400

    prototype release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -6,6 +6,15 @@
 #   $BINTRAY_USER
 #   $BINTRAY_PASS
 #   $RELEASE_TRIGGER
+if [ -z "$BINTRAY_USER" ] || [ -z "$BINTRAY_PASS" ]; then
+    echo "Missing BINTRAY_USER or BINTRAY_PASS environment variable! Exiting..."
+    exit 1
+fi
+
+# Publish to Bintray. Will do nothing by default for safety unless overridden
+# via RELEASE_TRIGGER=true.
 if [ "$RELEASE_TRIGGER" = "true" ]; then
-    sbt 'release with-defaults'
+    sbt release
+else
+    echo "Not really releasing, set RELEASE_TRIGGER=true to go live."
 fi
