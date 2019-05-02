684647f3ed77d1035ab7ca770a60913a3832f3fd
David Roon <david@roon.me>
2019-05-02 16:32:58 +0200

    circe reorg so cleanup serilization logic in the backend (#119)

--- shared/src/main/scala/org/adridadou/openlaw/parser/contract/ParagraphEdits.scala
+++ shared/src/main/scala/org/adridadou/openlaw/parser/contract/ParagraphEdits.scala
@@ -1,3 +1,11 @@
 package org.adridadou.openlaw.parser.contract
 
+import io.circe.generic.semiauto._
+import io.circe.{Decoder, Encoder}
+
 case class ParagraphEdits(edits:Map[Int, String] = Map())
+
+object ParagraphEdits {
+  implicit val paragraphEditsEnc:Encoder[ParagraphEdits] = deriveEncoder[ParagraphEdits]
+  implicit val paragraphEditsDec:Decoder[ParagraphEdits] = deriveDecoder[ParagraphEdits]
+}
--- version.sbt
+++ version.sbt
@@ -1,1 +1,1 @@
-version in ThisBuild := "0.1.29"
+version in ThisBuild := "0.1.30-SNAPSHOT"
