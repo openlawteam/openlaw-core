fbd9cb03af06c87a86718b5ebd707200d793003b
David Roon <david.roon@consensys.net>
2019-05-13 11:44:09 +0200

    implement OLOwnType

--- shared/src/main/scala/org/adridadou/openlaw/parser/template/variableTypes/OLOwnType.scala
+++ shared/src/main/scala/org/adridadou/openlaw/parser/template/variableTypes/OLOwnType.scala
@@ -1,15 +1,24 @@
 package org.adridadou.openlaw.parser.template.variableTypes
 import org.adridadou.openlaw.parser.template.TemplateExecutionResult
 import org.adridadou.openlaw.values.ContractId
+import io.circe.{Decoder, Encoder}
+import io.circe.generic.semiauto.{deriveDecoder, deriveEncoder}
+import io.circe.syntax._
+import io.circe.parser._
 
-case object OLOwnType extends VariableType("this") {
+case object OLOwnType extends VariableType("this") with NoShowInForm {
 
-  override def cast(value: String, executionResult: TemplateExecutionResult): Any = ???
+  override def cast(value: String, executionResult: TemplateExecutionResult): OLInformation = handleEither(decode[OLInformation](value))
 
-  override def internalFormat(value: Any): String = ???
+  override def internalFormat(value: Any): String = VariableType.convert[OLInformation](value).asJson.noSpaces
 
   override def thisType: VariableType = OLOwnType
   override def getTypeClass: Class[_] = classOf[OLInformation]
+}
+
+object OLInformation {
+  implicit val olInformationEnc:Encoder[OLInformation] = deriveEncoder[OLInformation]
+  implicit val olInformationDec:Decoder[OLInformation] = deriveDecoder[OLInformation]
 }
 
 case class OLInformation(id:Option[ContractId])
