e73dbe440d99b85722eb25d1287805a31c4ba7c4
Craig Blake <craig.blake@consensys.net>
2019-05-13 13:43:42 -0400

    Implement more members.

--- shared/src/main/scala/org/adridadou/openlaw/package.scala
+++ shared/src/main/scala/org/adridadou/openlaw/package.scala
@@ -11,6 +11,7 @@ package object openlaw {
   object OpenlawString {
     def unapply(string: OpenlawString): Option[String] = Some(string.string)
   }
+
   object OpenlawBigDecimal {
     def unapply(bigDecimal: OpenlawBigDecimal): Option[BigDecimal] = Some(bigDecimal.bigDecimal)
   }
@@ -24,17 +25,32 @@ package object openlaw {
   implicit def unwrap(bigDecimal: OpenlawBigDecimal): BigDecimal = bigDecimal.bigDecimal
   implicit def unwrap(int: OpenlawInt): Int = int.int
 
-  implicit class OpenlawBoolean(val boolean: Boolean) extends OpenlawValue
+  implicit class OpenlawBoolean(val boolean: Boolean) extends Comparable[OpenlawBoolean] with OpenlawValue {
+    override def toString: String = boolean.toString
+    override def compareTo(t: OpenlawBoolean): Int = boolean.compareTo(t.boolean)
+  }
+
   implicit class OpenlawString(val string: String) extends Comparable[OpenlawString] with OpenlawValue {
     override def toString: String = string
     override def compareTo(t: OpenlawString): Int = string.compareTo(t.string)
   }
-  implicit class OpenlawInt(val int: Int) extends OpenlawValue
-  //implicit class OpenlawInteger(val int: Integer) extends OpenlawValue
+
+  implicit class OpenlawInt(val int: Int) extends Comparable[OpenlawInt] with OpenlawValue {
+    override def toString: String = int.toString
+    override def compareTo(t: OpenlawInt): Int = int.compareTo(t.int)
+  }
+
   implicit class OpenlawBigDecimal(val bigDecimal: BigDecimal) extends Comparable[OpenlawBigDecimal] with OpenlawValue {
+    override def toString: String = bigDecimal.toString
     override def compareTo(t: OpenlawBigDecimal): Int = bigDecimal.compareTo(t.bigDecimal)
   }
-  implicit class OpenlawDateTime(val localDateTime: LocalDateTime) extends OpenlawValue
+
+  implicit class OpenlawDateTime(val localDateTime: LocalDateTime) extends Comparable[OpenlawDateTime] with OpenlawValue {
+    override def toString: String = localDateTime.toString
+    override def compareTo(t: OpenlawDateTime): Int = localDateTime.compareTo(t.localDateTime)
+  }
+
   implicit class OpenlawMap[T, Y <: OpenlawValue](val map: Map[T, Y]) extends OpenlawValue
-  implicit class OpenlawElements(val seq: Seq[AgreementElement]) extends OpenlawValue
+
+  implicit class OpenlawElements(val seq: Seq[AgreementElement]) extends OpenlawValue // TODO: The AbstractStructure handling should be refactored to not require returning this type in access
 }
