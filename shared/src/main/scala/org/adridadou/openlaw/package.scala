c7886efb27e61324112b7afc3b5d6800773f954a
Craig Blake <craig.blake@consensys.net>
2019-06-06 12:40:17 -0400

    Fixing equality and hashcode.

--- shared/src/main/scala/org/adridadou/openlaw/package.scala
+++ shared/src/main/scala/org/adridadou/openlaw/package.scala
@@ -11,18 +11,12 @@ package object openlaw {
   trait OpenlawValue {
     type T
     val underlying: T
-
-    override def equals(o: Any): Boolean = o match {
-      case value: OpenlawValue => underlying.equals(value.underlying)
-      case _ => false
-    }
-
-    override def hashCode(): Int = underlying.hashCode
   }
 
   trait OpenlawNativeValue extends OpenlawValue {
     type T = this.type
     val underlying: T = this
+
   }
 
   object OpenlawString {
@@ -47,36 +41,66 @@ package object openlaw {
     override type T = Boolean
     override def toString: String = underlying.toString
     override def compareTo(t: OpenlawBoolean): Int = underlying.compareTo(t.underlying)
+    override def equals(o: Any): Boolean = o match {
+      case value: OpenlawValue => underlying.equals(value.underlying)
+      case _ => underlying.equals(o)
+    }
+    override def hashCode: Int = underlying.hashCode
   }
 
   implicit class OpenlawString(override val underlying: String) extends Comparable[OpenlawString] with OpenlawValue {
     override type T = String
     override def toString: String = underlying
     override def compareTo(t: OpenlawString): Int = underlying.compareTo(t.underlying)
+    override def equals(o: Any): Boolean = o match {
+      case value: OpenlawValue => underlying.equals(value.underlying)
+      case _ => underlying.equals(o)
+    }
+    override def hashCode: Int = underlying.hashCode
   }
 
   implicit class OpenlawInt(override val underlying: Int) extends Comparable[OpenlawInt] with OpenlawValue {
     override type T = Int
     override def toString: String = underlying.toString
     override def compareTo(t: OpenlawInt): Int = underlying.compareTo(t.underlying)
+    override def equals(o: Any): Boolean = o match {
+      case value: OpenlawValue => underlying.equals(value.underlying)
+      case _ => underlying.equals(o)
+    }
+    override def hashCode: Int = underlying.hashCode
   }
 
   implicit class OpenlawBigDecimal(override val underlying: BigDecimal) extends Comparable[OpenlawBigDecimal] with OpenlawValue {
     override type T = BigDecimal
     override def toString: String = underlying.toString
     override def compareTo(t: OpenlawBigDecimal): Int = underlying.compareTo(t.underlying)
+    override def equals(o: Any): Boolean = o match {
+      case value: OpenlawValue => underlying.equals(value.underlying)
+      case _ => underlying.equals(o)
+    }
+    override def hashCode: Int = underlying.hashCode
   }
 
   implicit class OpenlawDateTime(override val underlying: LocalDateTime) extends Comparable[OpenlawDateTime] with OpenlawValue {
     override type T = LocalDateTime
     override def toString: String = underlying.toString
     override def compareTo(t: OpenlawDateTime): Int = underlying.compareTo(t.underlying)
+    override def equals(o: Any): Boolean = o match {
+      case value: OpenlawValue => underlying.equals(value.underlying)
+      case _ => underlying.equals(o)
+    }
+    override def hashCode: Int = underlying.hashCode
   }
 
   implicit class OpenlawMap[U, Y <: OpenlawValue](override val underlying: Map[U, Y]) extends Comparable[Map[U, Y]] with OpenlawValue {
     override type T = Map[U, Y]
     override def toString: String = underlying.toString
     override def compareTo(t: Map[U, Y]): Int = underlying.compareTo(t.underlying)
+    override def equals(o: Any): Boolean = o match {
+      case value: OpenlawValue => underlying.equals(value.underlying)
+      case _ => underlying.equals(o)
+    }
+    override def hashCode: Int = underlying.hashCode
   }
 
   // TODO: The AbstractStructure handling should be refactored to not require returning this type in access
@@ -84,5 +108,10 @@ package object openlaw {
     override type T = Seq[AgreementElement]
     override def toString: String = underlying.toString
     override def compareTo(t: Seq[AgreementElement]): Int = underlying.compareTo(t.underlying)
+    override def equals(o: Any): Boolean = o match {
+      case value: OpenlawValue => underlying.equals(value.underlying)
+      case _ => underlying.equals(o)
+    }
+    override def hashCode: Int = underlying.hashCode
   }
 }
