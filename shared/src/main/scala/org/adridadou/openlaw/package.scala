86b6ef92fc7157165dec41eb2a516edb2a05f6a6
Craig Blake <40273884+craigwblake-openlaw@users.noreply.github.com>
2019-06-06 17:18:01 -0400

    Implement universal implicit extraction of underlying type, and stand… (#146)
    * Implement universal implicit extraction of underlying type, and standard equality, with tests.
    
    * Fixing equality and hashcode.
    
    * Fix test in ScalaJs.
    
    * Remove change to unrelated test.

--- shared/src/main/scala/org/adridadou/openlaw/package.scala
+++ shared/src/main/scala/org/adridadou/openlaw/package.scala
@@ -16,59 +16,91 @@ package object openlaw {
   trait OpenlawNativeValue extends OpenlawValue {
     type T = this.type
     val underlying: T = this
+
   }
 
   object OpenlawString {
-    def unapply(string: OpenlawString): Option[String] = Some(string.underlying)
-  }
-
-  object OpenlawBigDecimal {
-    def unapply(bigDecimal: OpenlawBigDecimal): Option[BigDecimal] = Some(bigDecimal.underlying)
+    def unapply(value: OpenlawString): Option[String] = Some(value.underlying)
   }
 
   object OpenlawBoolean {
-    def unapply(boolean: OpenlawBoolean): Option[Boolean] = Some(boolean.underlying)
+    def unapply(value: OpenlawBoolean): Option[Boolean] = Some(value.underlying)
   }
 
-  implicit def unwrap(string: OpenlawString): String = string.underlying
-  implicit def unwrap(boolean: OpenlawBoolean): Boolean = boolean.underlying
-  implicit def unwrap(bigDecimal: OpenlawBigDecimal): BigDecimal = bigDecimal.underlying
-  implicit def unwrap(int: OpenlawInt): Int = int.underlying
+  object OpenlawBigDecimal {
+    def unapply(value: OpenlawBigDecimal): Option[BigDecimal] = Some(value.underlying)
+  }
+
+  object OpenlawDateTime {
+    def unapply(value: OpenlawDateTime): Option[LocalDateTime] = Some(value.underlying)
+  }
+
+  implicit def unwrap[U <: OpenlawValue](value: U): U#T = value.underlying
 
   implicit class OpenlawBoolean(override val underlying: Boolean) extends Comparable[OpenlawBoolean] with OpenlawValue {
     override type T = Boolean
     override def toString: String = underlying.toString
     override def compareTo(t: OpenlawBoolean): Int = underlying.compareTo(t.underlying)
+    override def equals(o: Any): Boolean = o match {
+      case value: OpenlawValue => underlying.equals(value.underlying)
+      case _ => underlying.equals(o)
+    }
+    override def hashCode: Int = underlying.hashCode
   }
 
   implicit class OpenlawString(override val underlying: String) extends Comparable[OpenlawString] with OpenlawValue {
     override type T = String
     override def toString: String = underlying
     override def compareTo(t: OpenlawString): Int = underlying.compareTo(t.underlying)
+    override def equals(o: Any): Boolean = o match {
+      case value: OpenlawValue => underlying.equals(value.underlying)
+      case _ => underlying.equals(o)
+    }
+    override def hashCode: Int = underlying.hashCode
   }
 
   implicit class OpenlawInt(override val underlying: Int) extends Comparable[OpenlawInt] with OpenlawValue {
     override type T = Int
     override def toString: String = underlying.toString
     override def compareTo(t: OpenlawInt): Int = underlying.compareTo(t.underlying)
+    override def equals(o: Any): Boolean = o match {
+      case value: OpenlawValue => underlying.equals(value.underlying)
+      case _ => underlying.equals(o)
+    }
+    override def hashCode: Int = underlying.hashCode
   }
 
   implicit class OpenlawBigDecimal(override val underlying: BigDecimal) extends Comparable[OpenlawBigDecimal] with OpenlawValue {
     override type T = BigDecimal
     override def toString: String = underlying.toString
     override def compareTo(t: OpenlawBigDecimal): Int = underlying.compareTo(t.underlying)
+    override def equals(o: Any): Boolean = o match {
+      case value: OpenlawValue => underlying.equals(value.underlying)
+      case _ => underlying.equals(o)
+    }
+    override def hashCode: Int = underlying.hashCode
   }
 
   implicit class OpenlawDateTime(override val underlying: LocalDateTime) extends Comparable[OpenlawDateTime] with OpenlawValue {
     override type T = LocalDateTime
     override def toString: String = underlying.toString
     override def compareTo(t: OpenlawDateTime): Int = underlying.compareTo(t.underlying)
+    override def equals(o: Any): Boolean = o match {
+      case value: OpenlawValue => underlying.equals(value.underlying)
+      case _ => underlying.equals(o)
+    }
+    override def hashCode: Int = underlying.hashCode
   }
 
   implicit class OpenlawMap[U, Y <: OpenlawValue](override val underlying: Map[U, Y]) extends Comparable[Map[U, Y]] with OpenlawValue {
     override type T = Map[U, Y]
     override def toString: String = underlying.toString
     override def compareTo(t: Map[U, Y]): Int = underlying.compareTo(t.underlying)
+    override def equals(o: Any): Boolean = o match {
+      case value: OpenlawValue => underlying.equals(value.underlying)
+      case _ => underlying.equals(o)
+    }
+    override def hashCode: Int = underlying.hashCode
   }
 
   // TODO: The AbstractStructure handling should be refactored to not require returning this type in access
@@ -76,5 +108,10 @@ package object openlaw {
     override type T = Seq[AgreementElement]
     override def toString: String = underlying.toString
     override def compareTo(t: Seq[AgreementElement]): Int = underlying.compareTo(t.underlying)
+    override def equals(o: Any): Boolean = o match {
+      case value: OpenlawValue => underlying.equals(value.underlying)
+      case _ => underlying.equals(o)
+    }
+    override def hashCode: Int = underlying.hashCode
   }
 }
