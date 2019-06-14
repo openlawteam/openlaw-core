4cd7ee0d7dcf561f8ec42f6524d2e629a25b3853
David Roon <david.roon@consensys.net>
2019-06-14 14:19:51 +0200

    fix number formatting

--- shared/src/test/scala/org/adridadou/openlaw/vm/OpenlawExecutionEngineSpec.scala
+++ shared/src/test/scala/org/adridadou/openlaw/vm/OpenlawExecutionEngineSpec.scala
@@ -1337,4 +1337,27 @@ class OpenlawExecutionEngineSpec extends FlatSpec with Matchers with OptionValue
       case Failure(ex, message) => fail(message ,ex)
     }
   }
+
+  it should "format small numbers properly" in {
+    val text =
+      """
+        [[my number:Number]]
+      """.stripMargin
+
+    val template = compile(text)
+
+    engine.execute(template, TemplateParameters("my number" -> "0.000000042")) match {
+      case Success(executionResult) =>
+        println(parser.forReview(executionResult.agreements.head))
+        parser.forReview(executionResult.agreements.head) shouldBe "<p class=\"no-section\"><br />        0.000000042<br />      </p>"
+      case Failure(ex, message) => fail(message ,ex)
+    }
+
+    engine.execute(template, TemplateParameters("my number" -> "4200000000")) match {
+      case Success(executionResult) =>
+        println(parser.forReview(executionResult.agreements.head))
+        parser.forReview(executionResult.agreements.head) shouldBe "<p class=\"no-section\"><br />        4,200,000,000<br />      </p>"
+      case Failure(ex, message) => fail(message ,ex)
+    }
+  }
 }
