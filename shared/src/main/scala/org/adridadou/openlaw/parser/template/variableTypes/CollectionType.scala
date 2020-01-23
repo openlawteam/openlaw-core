adcfae5eae7e2f04d6b3f5d4cac1f43634888830
david roon <david@roon.me>
2020-01-23 16:26:24 +0100

    collection.map was only working for String. it works with every type now

--- shared/src/main/scala/org/adridadou/openlaw/parser/template/variableTypes/CollectionType.scala
+++ shared/src/main/scala/org/adridadou/openlaw/parser/template/variableTypes/CollectionType.scala
@@ -28,7 +28,7 @@ case object AbstractCollectionType extends VariableType("Collection") with Param
     CollectionType(typeParameter)
 }
 
-final case class CollectionValue(size:Int = 1, values:Map[Int, OpenlawValue] = Map(), collectionType:CollectionType) extends OpenlawNativeValue {
+final case class CollectionValue(size:Int = 1, values:Map[Int, OpenlawValue] = Map.empty, collectionType:CollectionType) extends OpenlawNativeValue {
   def castValue(value:String, executionResult: TemplateExecutionResult):Result[OpenlawValue] = collectionType.typeParameter.cast(value, executionResult)
   def valueInternalFormat(value:OpenlawValue): Result[String] = collectionType.typeParameter.internalFormat(value)
   def list:List[OpenlawValue] = values.values.toList
@@ -54,12 +54,11 @@ final case class CollectionType(typeParameter:VariableType) extends VariableType
 
   override def defaultFormatter: Formatter = new NoopFormatter
 
-  override def internalFormat(value: OpenlawValue): Result[String] = {
+  override def internalFormat(value: OpenlawValue): Result[String] =
     for {
       collection <- VariableType.convert[CollectionValue](value)
       values <- collection.values.map({ case (key, v) => typeParameter.internalFormat(v).map(p => key -> p) }).toList.sequence
     } yield CollectionTypeValue(values = values.toMap, size = collection.size).asJson.noSpaces
-  }
 
   override def getTypeClass: Class[_ <: CollectionValue] = classOf[CollectionValue]
 
@@ -82,10 +81,13 @@ final case class CollectionType(typeParameter:VariableType) extends VariableType
     }
   }
 
-  private def applyMap(value:OpenlawValue, func:OLFunction, executionResult: TemplateExecutionResult):Result[Option[OpenlawValue]] = for {
-    ier <- executionResult.withVariable(func.parameter.name, value, func.parameter.varType(executionResult))
-    newValue <- func.expression.evaluate(ier)
-  } yield newValue
+  private def applyMap(value:OpenlawValue, func:OLFunction, executionResult: TemplateExecutionResult):Result[Option[OpenlawValue]] = {
+    println(s"parameter type ${func.parameter.varType(executionResult)}")
+    for {
+      ier <- executionResult.withVariable(func.parameter.name, value, typeParameter)
+      newValue <- func.expression.evaluate(ier)
+    } yield newValue
+  }
 
   override def keysType(keys: List[VariableMemberKey], expression: Expression, executionResult: TemplateExecutionResult): Result[VariableType] =
     keys match {
@@ -100,7 +102,7 @@ final case class CollectionType(typeParameter:VariableType) extends VariableType
   override def validateKeys(name:VariableName, keys: List[VariableMemberKey], expression:Expression, executionResult: TemplateExecutionResult): Result[Unit] = keys match {
     case Nil =>
       Success.unit
-    case VariableMemberKey(Right(OLFunctionCall(VariableName("map"), func:OLFunction)))::Nil =>
+    case VariableMemberKey(Right(OLFunctionCall(VariableName("map"), _:OLFunction)))::Nil =>
       Success.unit
     case _ =>
       Failure(s"property '${keys.mkString(".")}' could not be resolved in collection type")
